a5d4d373e2f4ab665af1e270f6a60765ed7542fc
Suraj Dubey <suraj.dubey@hotmail.com>
2021-05-02 21:08:25 +0530

    added .gitignore file

---
+++ src/main/java/com/example/ExampleApplication.java
@@ -0,0 +1,34 @@
+package com.example;
+
+import io.swagger.jaxrs.config.BeanConfig;
+
+import javax.annotation.PostConstruct;
+import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
+
+public class ExampleApplication extends Application {
+    public ExampleApplication() {
+        BeanConfig beanConfig = new BeanConfig();
+        beanConfig.setVersion("1.0.2");
+        beanConfig.setSchemes(new String[]{"http"});
+        beanConfig.setHost("localhost:3000");
+        beanConfig.setBasePath("/api");
+        beanConfig.setResourcePackage("com.example");
+        beanConfig.setScan(true);
+    }
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> resources = new HashSet();
+
+        resources.add(TestService.class);
+        //resources.add(SecondResource.class);
+        //...
+
+        resources.add(io.swagger.jaxrs.listing.ApiListingResource.class);
+        resources.add(io.swagger.jaxrs.listing.SwaggerSerializers.class);
+
+        return resources;
+    }
+}
