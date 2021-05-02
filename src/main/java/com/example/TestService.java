af2937b9fa9f277c42a2c7f7f4b0580ff57878e7
Suraj Dubey <suraj.dubey@hotmail.com>
2021-05-02 21:11:18 +0530

    removed target/ folder from git

--- src/main/java/com/example/TestService.java
+++ src/main/java/com/example/TestService.java
@@ -1,5 +1,8 @@
 package com.example;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -7,12 +10,14 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 @Path("/user")
+@Api(value = "User Api")
 //@Consumes(MediaType.APPLICATION_JSON)
 //@Produces(MediaType.APPLICATION_JSON)
 public class TestService {
 
     @GET
     @Path("/")
+    @ApiOperation(value = "Get user details")
     @Produces(MediaType.TEXT_PLAIN)
     public String getUser() {
         return "You have reached the endpoint";
