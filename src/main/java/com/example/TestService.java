fbe47562dc1210ae672b3be0f98f0916b144cfeb
Suraj Dubey <suraj.dubey@hotmail.com>
2021-12-27 14:03:50 +0530

    return dummy json response from getUser API

--- src/main/java/com/example/TestService.java
+++ src/main/java/com/example/TestService.java
@@ -1,8 +1,10 @@
 package com.example;
 
+import com.example.model.GetUserDetailsResponse;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -10,15 +12,15 @@ import javax.ws.rs.core.MediaType;
 
 @Path("/user")
 @Api(value = "User Api")
-//@Consumes(MediaType.APPLICATION_JSON)
-//@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class TestService {
 
     @GET
     @Path("/")
     @ApiOperation(value = "Get user details")
-    @Produces(MediaType.TEXT_PLAIN)
-    public String getUser() {
-        return "You have reached the endpoint";
+    @Produces(MediaType.APPLICATION_JSON)
+    public GetUserDetailsResponse getUser() {
+        return new GetUserDetailsResponse(1, "Suraj");
     }
 }
