8a34620928bd235a7d618d788d4a9506ed1f8114
Suraj Dubey <suraj.dubey@hotmail.com>
2022-02-03 20:25:44 +0530

    added slf4j dependencies

--- src/main/java/com/example/resources/ServerResponseLoggingFilter.java
+++ src/main/java/com/example/resources/ServerResponseLoggingFilter.java
@@ -1,13 +1,19 @@
 package com.example.resources;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
 import java.io.IOException;
 
+@Slf4j
 public class ServerResponseLoggingFilter implements ContainerResponseFilter {
     @Override
     public void filter(ContainerRequestContext containerRequestContext, ContainerResponseContext containerResponseContext) throws IOException {
+        Logger logger = LoggerFactory.getLogger("REQUEST");
         String newLine = System.lineSeparator();
         StringBuilder b = new StringBuilder();
         b.append(newLine);
@@ -23,7 +29,8 @@ public class ServerResponseLoggingFilter implements ContainerResponseFilter {
                 .append(containerRequestContext.getUriInfo().getRequestUri().toASCIIString())
                 .append(newLine)
                 .append(containerResponseContext.getEntity());
-
-        System.out.println(b);
+        logger.error(b.toString());
+        log.error(b.toString());
+//        System.out.println(b);
     }
 }
